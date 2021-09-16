6c42896860522063fc7ce674d10c77359969de07
Santhosh Krishnamoorthy <santhosh.k@Santhoshs-MacBook-Pro.local>
2021-09-16 19:09:06 +0530

    enable ansible

--- doom/init.el
+++ doom/init.el
@@ -92,10 +92,11 @@
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
-       ;;ansible
+       (ansible
+         +lsp)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       (docker 
+       (docker
 	 +lsp)
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
